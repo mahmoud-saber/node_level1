9efc3354333866e4dd5f52eb2efba06ac08260c1
mahmoud-saber <60474033+mahmoud-saber@users.noreply.github.com>
2023-11-27 02:12:54 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express =require('express')
+const app = express()
+const port = 3000
+
+app.get('/',(req,res)=>{
+    // res.send('HELLO WORLD!')
+    res.sendFile("./views/home.html",{root: __dirname})
+})
+    
+
+
+
+app.listen(port,()=>{
+    console.log(`http://localhost:${port}`)
+})
