d9b3c0ed95894e522797d48f23a4a1e468ec4be4
mahmoud-saber <60474033+mahmoud-saber@users.noreply.github.com>
2023-11-27 04:37:04 +0200

    lesson2

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express =require('express')
+const { default: mongoose } = require('mongoose')
 const app = express()
 const port = 3000
-
-app.get('/',(req,res)=>{
-    // res.send('HELLO WORLD!')
-    res.sendFile("./views/home.html",{root: __dirname})
-})
-    
 
 
 
 app.listen(port,()=>{
-    console.log(`http://localhost:${port}`)
+    console.log(`http://localhost:3000/`)
 })
+mongoose.connect("mongodb+srv://mahmoud:bqPQtQFbD3lw9efx@cluster0.evvlfpw.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+    app.get('/',(req,res)=>{
+    // res.send('HELLO WORLD!')
+    res.sendFile("./views/home.html")
+})
+    
+}).catch((error)=>{console.log(error)})
+// 
